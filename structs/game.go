23a6eb5e8676a7c30b4c480afc26bd3fa8150c82
White_ONyme <112949958+DannyLap@users.noreply.github.com>
2023-10-03 11:04:12 +0200

    update game file

--- structs/game.go
+++ structs/game.go
@@ -1,18 +1,37 @@
 package structs
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-type Game struct{}
+type Game struct {
+	ScreenT [32][64]byte
+}
 
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	purpleCol := color.RGBA{255, 0, 255, 255}
+	for i := 0; i < len(g.ScreenT); i++ {
+		for j := 0; j < len(g.ScreenT[i]); j++ {
+			if i == j {
+				g.ScreenT[i][j] = 1
+			}
+		}
+	}
+
+	for i := 0; i < len(g.ScreenT); i++ {
+		for j := 0; j < len(g.ScreenT[i]); j++ {
+			if g.ScreenT[i][j] == 1 {
+				screen.Set(i*10, j*10, purpleCol)
+			}
+		}
+	}
 	//ebitenutil.DebugPrint(screen, "Hello, World!")
 }
 
@@ -21,6 +40,17 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func OpenWindowEbiten() {
+	//var screen [32][64]byte
+	//for i := 0; i < len(screen); i++ {
+	//	for j := 0; j < len(screen[i]); j++ {
+	//		if i == j {
+	//			screen[i][j] = 1
+	//		}
+	//	}
+	//	//fmt.Print(i)
+	//	//fmt.Println(screen[i])
+	//}
+
 	ebiten.SetWindowSize(640, 320)
 	ebiten.SetWindowTitle("Chip 8")
 	if err := ebiten.RunGame(&Game{}); err != nil {
